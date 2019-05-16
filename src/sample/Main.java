2e973783b20c0bea387061fed8639dec85f6fee9
YeongHyeonKim <kwxyk@naver.com>
2019-05-16 15:43:28 +0900

    first commit

---
+++ src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Font.loadFont(getClass().getResourceAsStream("Roboto-Regular.ttf"), 14);
+
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("DDOKDDOK Chatbot");
+        primaryStage.setScene(new Scene(root, 640, 360));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/sample/Youtube.java
@@ -0,0 +1,20 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javax.swing.text.html.ImageView;
+
+public class Youtube implements Initializable {
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
